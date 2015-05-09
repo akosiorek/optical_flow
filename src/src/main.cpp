1d12d19d3c700d20a35f7c3acd4f55b72433dda6
Johannes Rausch <JohRausch@github.com>
2015-05-09 13:53:32 +0200

    Added Sequential Buffer Test

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-//#include <memory>
+#include <memory>
 
 class MyClass {};
 
---
+++ src/test/SequentialBufferTest.cpp
@@ -0,0 +1,97 @@
+/*
+ * test.cpp
+ *
+ *  Created on: May 28, 2014
+ *      Author: Adam Kosiorek
+ */
+
+
+#include "gtest/gtest.h"
+#include <queue>
+
+class Event{
+public:
+//    Event(){
+//        x=0;
+//        y=0;
+//        time=0;
+//        edge=0;
+//
+//    };
+
+    Event(unsigned int x, unsigned int y, unsigned time, int edge):x_(x),y_(y),time_(time),edge_(edge){
+    };
+    Event():Event(0,0,0,0){
+    };
+
+    unsigned int x_;
+    unsigned int y_;
+    unsigned int time_;
+    int edge_;
+};
+
+class SequentialBuffer{
+public:
+    SequentialBuffer(){
+    };
+    void push(Event event);
+    Event pop();
+
+    std::queue<Event> eventQueue_;
+
+//    void init();
+
+};
+
+void SequentialBuffer::push(Event event) {
+    eventQueue_.push(event);
+
+}
+
+Event SequentialBuffer::pop(){
+    Event res =eventQueue_.front();
+    eventQueue_.pop();
+    return res;
+}
+
+//void SequentialBuffer::init(){
+//
+//}
+
+
+class EventTest : public testing::Test {
+
+
+};
+
+
+TEST_F(EventTest, DefaultConstructorTest) {
+    Event e;
+    GTEST_ASSERT_EQ(e.x_, 0);
+    GTEST_ASSERT_EQ(e.y_, 0);
+    GTEST_ASSERT_EQ(e.time_, 0);
+    GTEST_ASSERT_EQ(e.edge_, 0);
+    Event e2 = Event(1,1,1,1);
+    GTEST_ASSERT_EQ(e2.x_, 1);
+    GTEST_ASSERT_EQ(e2.y_, 1);
+    GTEST_ASSERT_EQ(e2.time_, 1);
+    GTEST_ASSERT_EQ(e2.edge_, 1);
+    SequentialBuffer b;
+    b.push(e);
+    b.push(e2);
+    GTEST_ASSERT_EQ(b.pop().x_, e.x_);
+
+}
+
+//class SequentialBufferTest : public testing::Test {
+//
+//
+//};
+//
+//
+//TEST_F(sequentialBufferTest, SomeTest) {
+//
+//
+//
+//}
+
