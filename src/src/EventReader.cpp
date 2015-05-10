d5ee9c7d1996dba38627d197c776fc60474420a3
David B. Adrian <dawidh.adrian@googlemail.com>
2015-05-10 04:44:09 +0200

    - Changed edvstools to forked repository (due to fixes) - Added initial edvsreader - Some minor fixes

--- src/src/EventReader.cpp
+++ src/src/EventReader.cpp
@@ -2,8 +2,3 @@
 // Created by David Adrian
 
 #include "EventReader.h"
-
-void EventReader::openURIStream(const std::string& uri)
-{
-	stream = Edvs::OpenEventStream(uri);
-}
--- src/src/EventReader.h
+++ src/src/EventReader.h
@@ -2,9 +2,17 @@
 #define EVENT_READER_H
 
 #include <string>
+#include <thread>
 
 #include "Edvs/event.h"
 #include "Edvs/EventStream.hpp"
+
+class Buffer
+{
+public:
+	Buffer() {}
+	~Buffer() {}
+};
 
 /**
  * @brief Provide easy access to a stream of event
@@ -14,14 +22,80 @@
 class EventReader
 {
 public:
-	EventReader();
-	~EventReader();
+	EventReader() : bufferSet_(false), uri_(std::string("")), running_(false) {}
+	~EventReader() {}
 
-	void openURIStream(const std::string& uri);
-	void startReadingEvents();
+	void setURI(const std::string& uri) { uri_ = uri; }
+	std::string getURI() const { return uri_; }
+
+	void setBuffer(std::shared_ptr<Buffer> buffer)
+	{
+		buffer_ = buffer;
+		bufferSet_ = true;
+	}
+
+	bool isBufferSet() { return bufferSet_; }
+
+	/**
+	 * @brief Opens stream and pushes events to Buffer
+	 * @details [long description]
+	 */
+	void startPublishing()
+	{
+		if(!openStream()) return;
+
+        if (!eventPublisher_)
+		{
+			running_ = true;
+			eventPublisher_ = std::make_shared<std::thread>(&EventReader::pollEventStream, this);
+        }
+	}
+
+	void stopPublishing()
+	{
+		if(running_)
+		{
+			running_ = false;
+			eventPublisher_->join();
+		}
+	}
+
+	bool isPublishing() { return running_; }
 
 private:
-	std::shared_ptr<Edvs::IEventStream> stream;
+	/**
+	 * @brief Opens stream and returns a bool if successful.
+	 */
+	bool openStream()
+	{
+		std::shared_ptr<Edvs::IEventStream> stream_ = Edvs::OpenEventStream(uri_);
+		return stream_->is_open();
+	}
+
+	void pollEventStream()
+	{
+	    // capture events (run until end of file or Ctrl+C)
+		while(!stream_->eos() && running_ == true)
+		{
+			// read events from stream
+			auto events = stream_->read();
+			// display message
+			if(!events.empty())
+			{
+				// TODO PUSH TO BUFFER
+			}
+		}
+	}
+
+
+	bool bufferSet_;
+	std::shared_ptr<Buffer> buffer_;
+
+	std::string uri_;
+
+	std::shared_ptr<Edvs::IEventStream> stream_;
+	std::shared_ptr<std::thread> eventPublisher_;
+	bool running_;
 	
 };
 
