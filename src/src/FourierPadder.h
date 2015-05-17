6bec77780cec0c406dd8c1b3d00e1de3920c0df3
David B. Adrian <dawidh.adrian@googlemail.com>
2015-05-17 15:43:00 +0200

    Correcting the intial class. Compiles now

--- src/src/FourierPadder.h
+++ src/src/FourierPadder.h
@@ -1,14 +1,44 @@
 //
 // Created by dadrian on 5/14/2015.
 //
+#ifndef FOURIER_PADDER_H
+#define FOURIER_PADDER_H
 
-uint32_t getNextPowerOfTwo(uint32_t n)
+#include <memory>
+#include <stdint.h>
+
+#include <Eigen/Core>
+#include <Eigen/SparseCore>
+
+class FourierPadder
 {
-    v--;
-    v |= v >> 1;
-    v |= v >> 2;
-    v |= v >> 4;
-    v |= v >> 8;
-    v |= v >> 16;
-    return ++v;
-}
+public:
+	FourierPadder(const int inputSize, const int filterSize) : 
+		inputSize_(inputSize), 
+		filterSize_(filterSize)
+	{
+		fourierSize_ = getNextPowerOfTwo(inputSize_+filterSize_-1);
+	}
+
+	std::shared_ptr<Eigen::Matrix<float, 15, 15> > 
+	padInput(std::shared_ptr<Eigen::SparseMatrix<float> > input);
+
+	uint32_t getNextPowerOfTwo(uint32_t n) const
+	{
+		n--;
+		n |= n >> 1;
+		n |= n >> 2;
+		n |= n >> 4;
+		n |= n >> 8;
+		n |= n >> 16;
+		return ++n;
+	}
+
+private:
+	const uint32_t inputSize_;
+	const uint32_t filterSize_;
+	uint32_t fourierSize_;
+
+};
+
+#endif // FOURIER_PADDER_H
