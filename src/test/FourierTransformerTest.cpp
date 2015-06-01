88ee961daeb1b3588e8a330199001cb5a72011da
David B. Adrian <dawidh.adrian@googlemail.com>
2015-06-01 18:31:54 +0200

    Added final test for FourierTransformer (and FourierPadder implicitly) doing full forward, cwise filtering, backward transformation

--- src/test/FourierTransformerTest.cpp
+++ src/test/FourierTransformerTest.cpp
@@ -8,6 +8,7 @@
 #include "utils.h"
 #include "IFourierTransformer.cpp"
 #include "FourierTransformerFFTW.cpp"
+#include "FourierPadder.h"
 
 class FourierTransformerTest : public testing::Test {
 
@@ -19,18 +20,18 @@ public:
     std::unique_ptr<FourierTransformerFFTW> ftfftw;
 };
 
-TEST_F(FourierTransformerTest, ConstructorTest) 
+TEST_F(FourierTransformerTest, ConstructorTest)
 {
 	ASSERT_EQ(8,ftfftw->rows_);
 	ASSERT_EQ(8,ftfftw->cols_);
 	ASSERT_EQ(8/2+1,ftfftw->colsHS_);
 }
 
-TEST_F(FourierTransformerTest, ForwardTestMatrixSizes) 
+TEST_F(FourierTransformerTest, ForwardTestMatrixSizes)
 {
 	RealMatrix 	inT(ftfftw->rows_,ftfftw->rows_),
 				inF(7,9);
-	ComplexMatrix 	outT(ftfftw->rows_,ftfftw->colsHS_), 
+	ComplexMatrix 	outT(ftfftw->rows_,ftfftw->colsHS_),
 				  	outF(3,3);
 
 	ASSERT_THROW(ftfftw->forward(inT,outF), std::invalid_argument);
@@ -39,9 +40,9 @@ TEST_F(FourierTransformerTest, ForwardTestMatrixSizes)
 	ASSERT_NO_THROW(ftfftw->forward(inT,outT));
 }
 
-TEST_F(FourierTransformerTest, BackwardTestMatrixSizes) 
+TEST_F(FourierTransformerTest, BackwardTestMatrixSizes)
 {
-	ComplexMatrix 	inT(ftfftw->rows_,ftfftw->colsHS_), 
+	ComplexMatrix 	inT(ftfftw->rows_,ftfftw->colsHS_),
 				  	inF(3,3);
 	RealMatrix 	outT(ftfftw->rows_,ftfftw->rows_),
 				outF(7,9);
@@ -52,10 +53,10 @@ TEST_F(FourierTransformerTest, BackwardTestMatrixSizes)
 	ASSERT_NO_THROW(ftfftw->backward(inT,outT));
 }
 
-TEST_F(FourierTransformerTest, FowardInputConsistency) 
+TEST_F(FourierTransformerTest, FowardInputConsistency)
 {
 	RealMatrix 		rm(ftfftw->rows_,ftfftw->rows_);
-		
+
 	rm << 	17	,24	,1	,8	,15	,0,	0,	0,
 			23	,5	,7	,14	,16	,0,	0,	0,
 			4	,6	,13	,20	,22	,0,	0,	0,
@@ -71,12 +72,12 @@ TEST_F(FourierTransformerTest, FowardInputConsistency)
 
 	ftfftw->forward(rm,cm);
 	ASSERT_EQ(true,rm.isApprox(rm_copy,0.000000001));
-}	
+}
 
-TEST_F(FourierTransformerTest, ForwardBackwardScaling) 
+TEST_F(FourierTransformerTest, ForwardBackwardScaling)
 {
 	RealMatrix 		rm(ftfftw->rows_,ftfftw->rows_);
-		
+
 	rm << 	17	,24	,1	,8	,15	,0,	0,	0,
 			23	,5	,7	,14	,16	,0,	0,	0,
 			4	,6	,13	,20	,22	,0,	0,	0,
@@ -85,14 +86,57 @@ TEST_F(FourierTransformerTest, ForwardBackwardScaling)
 			0	,0	,0	,0	,0	,0,	0,	0,
 			0	,0	,0	,0	,0	,0,	0,	0,
 			0	,0	,0	,0	,0	,0,	0,	0;
-	RealMatrix		rm_copy = rm;			
+	RealMatrix		rm_copy = rm;
 
 	ComplexMatrix	cm(ftfftw->rows_,ftfftw->cols_/2+1);
 	cm.setZero();
 
-	ftfftw->forward(rm,cm);	
+	ftfftw->forward(rm,cm);
 	ftfftw->backward(cm,rm);
 	rm = rm/(ftfftw->rows_*ftfftw->cols_);
 
 	ASSERT_EQ(true,rm.isApprox(rm_copy,0.000001));
-}	
+}
+
+TEST_F(FourierTransformerTest, FwdBwdFilterTest)
+{
+	RealMatrix	rmData(5,5), rmFilter(3,3);
+
+	rmData << 	17, 24, 1, 8, 15,
+			23, 5, 7, 14, 16,
+			4, 6, 13, 20, 22,
+			10, 12, 19, 21, 3,
+			11, 18, 25, 2, 9;
+
+	rmFilter << 8,	1,	6,
+				3,	5,	7,
+				4,	9,	2;
+
+	RealMatrix		rmDataPadded, rmFilterPadded;
+	FourierPadder padder(5, 3);
+	padder.padData(rmData,rmDataPadded);
+	padder.padFilter(rmFilter,rmFilterPadded);
+
+	ComplexMatrix	cmData(ftfftw->rows_,ftfftw->cols_/2+1);
+	ComplexMatrix	cmFilter(ftfftw->rows_,ftfftw->cols_/2+1);	
+	cmData.setZero(); 	cmFilter.setZero();
+
+	ftfftw->forward(rmDataPadded,cmData);
+	ftfftw->forward(rmFilterPadded,cmFilter);
+
+	ComplexMatrix cmResult = cmData.cwiseProduct(cmFilter);
+
+	ftfftw->backward(cmResult,rmDataPadded);
+	rmDataPadded = rmDataPadded/(ftfftw->rows_*ftfftw->cols_);
+	padder.extractDenseOutput(rmDataPadded,rmData);
+
+	RealMatrix expectedResult(5,5);
+	expectedResult << 	220, 441, 346, 276, 231,
+						431, 595, 410, 575, 471,
+						371, 440, 555, 620, 551,
+						301, 585, 600, 765, 421,
+						247, 446, 536, 451, 128;
+
+	std::cout << rmData << std::endl;
+	ASSERT_EQ(true,rmData.isApprox(expectedResult,0.000001));
+}
