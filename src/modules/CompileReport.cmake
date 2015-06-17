eda7f4975ea9f5c330672b33a2675b8d29ddbefb
David B. Adrian <dawidh.adrian@googlemail.com>
2015-06-17 17:54:24 +0200

    Improved and fixed report latex compiling

---
+++ src/modules/CompileReport.cmake
@@ -0,0 +1,53 @@
+# Just an include to make main file cleaner
+set(REPORT_DIR ${CMAKE_SOURCE_DIR}/../report)
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/../report/ebof.tex ${CMAKE_CURRENT_BINARY_DIR}/ebof.tex @ONLY)
+  add_custom_target(step1
+  COMMAND   ${LATEX_COMPILER} ${CMAKE_CURRENT_BINARY_DIR}/ebof.tex
+  DEPENDS   ${CMAKE_CURRENT_BINARY_DIR}/ebof.tex
+  COMMENT   "STEP1"
+  )
+
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/../report/ebof.bib ${CMAKE_CURRENT_BINARY_DIR}/ebof.bib @ONLY)
+  add_custom_target(step2
+  COMMAND   ${BIBTEX_COMPILER} ebof
+  DEPENDS   ${CMAKE_CURRENT_BINARY_DIR}/ebof.aux
+        ${CMAKE_CURRENT_BINARY_DIR}/ebof.bib
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+  COMMENT   "STEP2: BIBTEX"
+  )
+  add_dependencies(step2 step1)
+
+  add_custom_target(step3
+  COMMAND   ${LATEX_COMPILER} ${CMAKE_CURRENT_BINARY_DIR}/ebof.tex
+  DEPENDS   ${CMAKE_CURRENT_BINARY_DIR}/ebof.tex
+  COMMENT   "STEP3"
+  )
+  add_dependencies(step3 step2)
+
+  add_custom_target(step4
+  COMMAND   ${LATEX_COMPILER} ${CMAKE_CURRENT_BINARY_DIR}/ebof.tex
+  DEPENDS   ${CMAKE_CURRENT_BINARY_DIR}/ebof.tex
+  COMMENT   "STEP4"
+  )
+  add_dependencies(step4 step3)
+
+  IF(DVIPS_CONVERTER)
+    add_custom_target(reportps
+      COMMAND   ${DVIPS_CONVERTER} ${CMAKE_CURRENT_BINARY_DIR}/ebof.dvi
+                -o ${CMAKE_CURRENT_BINARY_DIR}/ebof.ps
+      DEPENDS   ${CMAKE_CURRENT_BINARY_DIR}/ebof.dvi
+      COMMENT   "STEP5"
+    )
+    add_dependencies(reportps step4)
+
+    IF(PS2PDF_CONVERTER)
+      add_custom_target(report
+      COMMAND   ${PS2PDF_CONVERTER} -sPAPERSIZE=a4 ${CMAKE_CURRENT_BINARY_DIR}/ebof.ps
+      DEPENDS   ${CMAKE_CURRENT_BINARY_DIR}/ebof.ps
+      COMMENT   "STEP6: Final PDF Report"
+      )
+      add_dependencies(report reportps)
+    ENDIF(PS2PDF_CONVERTER)
+  ENDIF(DVIPS_CONVERTER)
